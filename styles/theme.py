418725fadf02fe64938e37a8ca261f9847c58101
marcosmoraleslee <con2mms2000@gmail.com>
2026-09-15 21:25:46 +0200

    Delete styles directory

--- styles/theme.py
+++
@@ -1,5 +0,0 @@
-from styles.custom_css import get_custom_css
-
-def apply_theme(st_instance):
-    custom_css = get_custom_css()
-    st_instance.markdown(f"<style>{custom_css}</style>", unsafe_allow_html=True)
